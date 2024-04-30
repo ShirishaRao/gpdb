502be92327f3493f7a584ad72ff677c74ee6942c
Shirisha SN <sshirisha@vmware.com>
2024-04-30 12:05:59 +0530

    [6X Backport] Stream WAL archiving status from primaries to mirrors
    Backported from 7x commit: c42ee49db3dcbcb42f5e82f958dae8664ada74f5
    with some conflicts.
    
    This commit introduces a GUC `wal_sender_archiving_status_interval`
    that will specify the interval at which primaries will send an
    archival status messages to their mirrors (including coordinator and
    standby coordinator). Any value greater than zero will enable this
    feature and set the interval.
    
    Currently, if a primary loses connectivity to its archive repository
    while the streaming replication between the primary and its mirror is
    intact, it is likely that there will be WAL files that the mirror has
    but the archive repository does not. If primary goes down at this
    stage, those WAL files will be lost forever as they are marked as
    .done on the mirror as part of regular streaming replication
    logic. When the mirror is promoted, it will start archiving from the
    last WAL file that was streamed over. As a result, there will be holes
    in the sequence of WAL files in the archive repository that may
    negatively affect future restores from the archive repository.
    
    With the GUC value set, the mirror will now mark all WAL files that
    are streamed over as .ready instead of .done. In parallel, the
    primary's WAL sender process will fetch the last archived WAL from the
    `pg_stat_archiver` view that gets updated by the primary's archiver
    process. This informartion is sent at regular intervals (set by the
    GUC) to the mirror's WAL receiver process via the existing WAL
    replication stream. Based on this information, the mirror will mark
    corresponding WAL files from .ready to .done status. When the mirror
    gets promoted, it may now have WAL files in .ready state which will
    then get archived accordingly.
    
    There is a possibility that these WAL files may have already been
    archived by the primary because the archiving status updates sent to
    the mirror are not real-time. However, trying to re-archive them
    should be safe as long as the archive_command GUC is configured
    accordingly to check if the file is already in the archive repository,
    compare the checksums if so, and skip the file if the checksums are
    the same.
    
    This patch was inspired by an unapplied patch from Heikki Linnakangas
    in the upstream pgsql-hackers thread:
    https://www.postgresql.org/message-id/5550D20D.6090703%40iki.fi
    
    There was a small bug in the original patch from Heikki. Essentially when
    pg_basebackup gets called with the -X stream option, it starts a wal sender and
    a wal receiver process. This wal sender is forked off the main wal sender
    process and will have the 'wal_receiver_status_interval' GUC enabled. Because
    of this, on startup, this wal sender process will send the archival report
    which will then be received by pg_basebackup's wal receiver. This report
    message wasn't handled by the original patch. We can safely ignore this message
    since it isn't really useful to pg_basebackup's wal receiver. Note that because
    of this change, gp_receivewal will also ignore this archive report which should
    be fine for now.
    
    Co-authored-by: Nikhil Kak <nkak@vmware.com>

--- src/include/access/xlog.h
+++ src/include/access/xlog.h
@@ -212,8 +212,12 @@ typedef enum WalLevel
 } WalLevel;
 extern int	wal_level;
 
+extern int wal_sender_archiving_status_interval;
 #define XLogArchivingActive()	(XLogArchiveMode && wal_level >= WAL_LEVEL_ARCHIVE)
 #define XLogArchiveCommandSet() (XLogArchiveCommand[0] != '\0')
+/* Is WAL archiving status streaming enabled? */
+#define XLogArchivingStatusReportingActive() \
+	(XLogArchivingActive() && wal_sender_archiving_status_interval > 0)
 
 /*
  * Is WAL-logging necessary for archival or log-shipping, or can we skip
